bbedef9e5113a4fc4b11a109cb7201b9f7ff5833
jezz jazmin <jezz.jazmin@outlook.com>
2022-04-22 16:26:10 -0700

    read commands from folder

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const { Op } = require('sequelize');
+const fs = require('node:fs');
 const { Collection, Client, Formatters, Intents } = require('discord.js');
-const { Users, UserInventories } = require('./db-objects.js');
 const { token } = require('./config.json');
 
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
-const currency = new Collection();
 
-client.once('ready', async () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
+client.commands = new Collection();
+const command_files = fs
+	.readdirSync('./commands')
+	.filter((file) => file.endsWith('.js'));
 
-client.on('messageCreate', async (message) => {
-	if (message.author.bot) return;
-	currency.add(message.author.id, 1);
-});
-
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isCommand()) return;
-
-	const { commandName } = interaction;
-});
+for (const file of command_files) {
+	const command = require(`./commands/${file}`);
+	//Set a new item in the Collection
+	//With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 client.login(token);
