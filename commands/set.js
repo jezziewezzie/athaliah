59b926a212eb134a97de9255e141eb87e9a4e266
jezz jazmin <jezz.jazmin@outlook.com>
2022-04-22 22:25:23 -0700

    get set working

--- commands/set.js
+++ commands/set.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Database = require('better-sqlite3');
+
 const db = new Database('db.sqlite3', { verbose: console.log });
 
 module.exports = {
@@ -8,29 +9,26 @@ module.exports = {
 		.setDescription('Set guild options')
 		.addSubcommand((subcommand) =>
 			subcommand
-				.setName('member role')
+				.setName('member_role')
 				.setDescription('Set the role that signifies membership')
 				.addRoleOption((option) =>
 					option.setName('role').setDescription('Select a role'),
 				),
 		),
 	async execute(interaction) {
-		if (interaction.options.getSubcommand() === 'member role') {
-			const guild_id = interaction.guild_id;
+		if (interaction.options.getSubcommand() === 'member_role') {
 			const member_role_id = interaction.options.getRole('role');
 
 			const stmt = db.prepare(`
-                REPLACE INTO TABLE guild_settings (guild_id, member_role_id)
+                REPLACE INTO guild_settings (guild_id, member_role_id)
                 VALUES(@guild_id, @member_role_id)
             `);
 			stmt.run({
-				guild_id: guild_id,
-				member_role_id: member_role_id,
+				guild_id: String(interaction.guild.id),
+				member_role_id: String(member_role_id),
 			});
 
-			return interaction.reply(
-				`Member role set to <@&${member_role_id}>!`,
-			);
+			return interaction.reply(`Member role set to ${member_role_id}!`);
 		}
 	},
 };
